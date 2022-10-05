dc886ce85b2568c7493c79ba3b262c21e45d85d3
Sahitya324 <sahityakumar324@gmail.com>
2022-10-05 11:30:40 +0530

    Updated Image Problem

--- seeds/index.js
+++ seeds/index.js
@@ -26,7 +26,7 @@ const seedDB = async () => {
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             //YOUR USER ID
-            author: '5f5c330c2cd79d538f2c66d9',
+            author: '632e0edd2cbef81308203b07',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
@@ -40,12 +40,12 @@ const seedDB = async () => {
             },
             images: [
                 {
-                    url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png',
-                    filename: 'YelpCamp/ahfnenvca4tha00h2ubt'
+                    url: 'https://res.cloudinary.com/dggiaolym/image/upload/v1663966484/YelpCamp/falghfp4mh1vwgasrto0.jpg',
+                    filename: 'YelpCamp/falghfp4mh1vwgasrto0'
                 },
                 {
-                    url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ruyoaxgf72nzpi4y6cdi.png',
-                    filename: 'YelpCamp/ruyoaxgf72nzpi4y6cdi'
+                    url: 'https://res.cloudinary.com/dggiaolym/image/upload/v1663966493/YelpCamp/seyenuatxd3jx3jpaqyt.jpg',
+                    filename: 'YelpCamp/seyenuatxd3jx3jpaqyt'
                 }
             ]
         })
